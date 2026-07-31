3e62f5cf9d06b15643d583c84c4a224af69375d5
J0k3r$ <maxriddle12347@gmail.com>
2026-07-31 19:50:42 +0700

    Commit: Add 3D displayer for helmet item

--- src/main/java/com/k1ngtle/taticalsuit/item/HelmetItem.java
+++ src/main/java/com/k1ngtle/taticalsuit/item/HelmetItem.java
@@ -1,7 +1,9 @@
 package com.k1ngtle.taticalsuit.item;
 
+import com.k1ngtle.taticalsuit.client.model.HelmetModel;
 import com.k1ngtle.taticalsuit.client.renderer.HelmetRenderer;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
@@ -11,6 +13,7 @@ import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.renderer.GeoItemRenderer;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 import java.util.function.Consumer;
@@ -22,26 +25,34 @@ public class HelmetItem extends ArmorItem implements GeoItem {
         super(material, type, properties);
     }
 
-    // This links the Item to your 3D Renderer!
     @Override
     public void initializeClient(Consumer<IClientItemExtensions> consumer) {
         consumer.accept(new IClientItemExtensions() {
-            private HelmetRenderer renderer;
+            private HelmetRenderer armorRenderer;
+            private GeoItemRenderer<HelmetItem> itemRenderer;
 
             @Override
             public HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
-                if (this.renderer == null) {
-                    this.renderer = new HelmetRenderer();
+                if (this.armorRenderer == null) {
+                    this.armorRenderer = new HelmetRenderer();
                 }
-                this.renderer.prepForRender(livingEntity, itemStack, equipmentSlot, original);
-                return this.renderer;
+                this.armorRenderer.prepForRender(livingEntity, itemStack, equipmentSlot, original);
+                return this.armorRenderer;
+            }
+
+            // THIS is what allows the 3D model to render in your hand and inventory GUI!
+            @Override
+            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                if (this.itemRenderer == null) {
+                    this.itemRenderer = new GeoItemRenderer<>(new HelmetModel());
+                }
+                return this.itemRenderer;
             }
         });
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        // Add controllers here if you eventually add animations
     }
 
     @Override
